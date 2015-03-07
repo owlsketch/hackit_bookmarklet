6de10c7108ae8dcca4b90d0977873b89888bfccc
Satimidus <satimidus@gmail.com>
2015-03-07 17:10:29 -0500

    Canvas element added, etc

--- hack.js
+++ hack.js
@@ -1,7 +1,19 @@
 //Since our bookmarklet isn't run immediatly, function doesn't need to wait
 //for DOM to be loaded
 var R4D1C4L = function () {
-    console.log("Hello World!");
+    var F4C3 = document.createElement('canvas');
+    F4C3.height = 100;
+    F4C3.width = 100;
+    F4C3.style.position = "absolute";
+    F4C3.style.zIndex = "1000000";
+    F4C3.style.visibility = "visible";
+    F4C3.style.left = "10px";
+    F4C3.style.top = "10px";
+
+
+    var cntx = F4C3.getContext("2d");
+    cntx.fillRect(5,5,90,90);
+    document.body.appendChild(F4C3);
 };
 
 R4D1C4L();
