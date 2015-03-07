dc7b3c472b8d198e988d388b69c2e9d1d3ac2a67
Satimidus <satimidus@gmail.com>
2015-03-07 15:48:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# hackit_bookmarklet
+Bookmarklet game that allows Edward Wong Hau Pepelu Tivrusky IV to hack your website.
+
+Made for HackNY Spring 2015~
---
+++ hack.js
@@ -0,0 +1,8 @@
+//Since our bookmarklet isn't run immediatly, function doesn't need to wait
+//for DOM to be loaded
+var R4D1C4L = function () {
+    console.log("Hello World!");
+};
+
+R4D1C4L();
+
